5c70ad62b5741b557cae8bd6309ed3b883e8b1a1
Greg Santos <gregsantos@gmail.com>
2018-08-02 12:26:14 -0700

    Add now-env for secrets

--- next.config.js
+++ next.config.js
@@ -2,7 +2,25 @@
 // const withCSS = require("@zeit/next-css");
 const images = require("remark-images");
 const emoji = require("remark-emoji");
+const webpack = require("webpack");
+/**
+ * After the next require you can use process.env to get your secrets
+ */
+if (process.env.NODE_ENV !== "production") {
+  require("now-env");
+}
 
+console.log({
+  SECRET: process.env.SECRET,
+  ANOTHER_SECRET: process.env.ANOTHER_SECRET,
+  SECRET_FAIL: process.env.SECRET_FAIL
+});
+
+/**
+ * If some of the envs are public, like a google maps key, but you still
+ * want to keep them secret from the repo, the following code will allow you
+ * to share some variables with the client, configured at compile time.
+ */
 module.exports = {
   pageExtensions: ["js", "jsx", "md", "mdx"],
   webpack: (config, { defaultLoaders }) => {
@@ -18,12 +36,18 @@ module.exports = {
         }
       ]
     });
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        "process.env.SECRET": JSON.stringify(process.env.SECRET)
+      })
+      // Same as above
+      // new webpack.EnvironmentPlugin(['SECRET'])
+    );
 
     return config;
   },
   webpackDevMiddleware: config => {
     // Perform customizations to webpack dev middleware config
-
     // Important: return the modified config
     return config;
   }
--- pages/about.js
+++ pages/about.js
@@ -8,6 +8,7 @@ const AboutPage = ({ theme }) => (
     <Box width={[1, 1 / 2, null, 1 / 3]} p={2} bg={theme.fourth}>
       <Card m={3} p={4}>
         <Text my={3} py={2} children="Isn't this great?" />
+        <strong>{process.env.SECRET}</strong>
         <Button
           w={[1, null, "7em"]}
           f={[".8em", ".9em", "1em"]}
